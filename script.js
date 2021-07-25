bcd3a9d5dd70a827eba6bdb5e0c1a2ba654716d8
Aman Singh <amanksofficial@gmail.com>
2021-07-25 12:48:38 +0530

    updated ui

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const quoteEl = document.querySelector(".quote__text");
-const animeEl = document.querySelector(".anime__text");
+const animeEl = document.querySelector(".anime__name");
 
 async function getQuote() {
     const res = await fetch("https://animechan.vercel.app/api/random");
@@ -11,12 +11,8 @@ async function main() {
     if (character.split(" ").length > 20 || anime.split(" ").length > 4) {
         main();
     } else {
-        quoteEl.innerHTML = `<span class="character">${character}</span>: <br>${quote}`;
-        animeEl.innerHTML = `<strong>Anime</strong>: ${anime}`;
+        quoteEl.innerHTML = `<span class="character">${character} 🚀</span> <p>${quote}</p>`;
+        animeEl.textContent = anime;
     }
 }
 main();
-
-document.querySelector(".refresh").addEventListener("click", () => {
-    main();
-});
