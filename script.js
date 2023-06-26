586730874454fe8f0ed0d152eaf4de2f20d682d0
Aman K. Singh <amanksofficial@outlook.com>
2023-06-27 02:55:12 +0530

    api endpoint update + error handling

--- script.js
+++ script.js
@@ -3,28 +3,35 @@ const animeEl = document.querySelector(".anime__name");
 const reloadBtn = document.getElementById("reload");
 
 async function getQuote() {
-    const res = await fetch("https://animechan.vercel.app/api/random");
-    return res.json();
+  const res = await fetch("https://animechan.xyz/api/random");
+  return res.json();
 }
 
 async function main() {
+  try {
     const { character, quote, anime } = await getQuote();
     if (character.split(" ").length > 20 || anime.split(" ").length > 4) {
-        main();
+      main();
     } else {
-        quoteEl.innerHTML = `<span class="character">${character} 🚀</span> <p>${quote}</p>`;
-        animeEl.textContent = anime;
+      quoteEl.innerHTML = `<span class="character">${character} 🚀</span> <p>${quote}</p>`;
+      animeEl.textContent = anime;
     }
+  } catch {
+    quoteEl.innerHTML = `<span style="font-size: 14px">🚨Uh oh! Looks like we KameHameHaa'd our server. <br/>
+        Sorry for the annoyance, we're working on it.
+        </span>`;
+    animeEl.textContent = `Not Found`;
+  }
 }
 main();
 
 reloadBtn.addEventListener("click", () => {
-    quoteEl.innerHTML = `
+  quoteEl.innerHTML = `
     <div class="spinner-border" role="status">
         <span class="sr-only"></span>
     </div>`;
 
-    animeEl.textContent = 'loading...';
+  animeEl.textContent = "loading...";
 
-    setTimeout(main, 500);
+  setTimeout(main, 500);
 });
